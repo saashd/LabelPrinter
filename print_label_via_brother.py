6bb119f3d56f8ad3090d47e7c66795ba4d8eed01
AlexDruzina <druzinashura1@gmail.com>
2020-08-09 08:56:40 +0300

    brother_ql, pyusb and libusb-win32-have to be installed.

---
+++ print_label_via_brother.py
@@ -0,0 +1,81 @@
+import os
+import sys
+
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+import time
+import json
+from flask import Flask, request, jsonify, Response
+
+# install brother_ql, pyusb and libusb-win32
+# type ipconfig in cmd to check ur IP adress
+
+# .py to .exe : install pyinstaller and write in cmd: pyinstaller --onefile print_label_via_brother.py
+# server to communicate with label printer
+
+
+app = Flask(__name__)
+
+#  http://127.0.0.1:5000/print?number=2&quantity=2 -this is how it works
+@app.route("/print",methods=['get'])
+def do_print():
+    number = request.args.get('number', None)  # use default value replace 'None'
+    quantity = request.args.get('quantity', None)
+    create_label(number)
+
+    if print_label('print.png',quantity)==0:
+        # return Response( {'status': 'success'},200,mimetype="text/html")
+        return jsonify( {'status': 'success'} ),200
+    return jsonify({'status': 'fail'}),500
+    # return Response( {'status': 'fail'},500,mimetype="text/html")
+
+
+
+
+
+#add serial number to template to create new label
+def create_label(serial_num):
+    in_file = 'template.png'
+    out_file = 'print.png'
+    year_last_2_digits = time.strftime("%y", time.localtime())
+    num = str(serial_num)
+    text = 'GW' + num + '/' + year_last_2_digits
+    img = Image.open(in_file)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("C:\Windows\Fonts\Calibri.ttf", 100)
+    draw.text((125, 125), text, fill="black", font=font, align='center')
+    img.save(out_file)
+
+# print label using brother_ql library
+def print_label(img,quantity):
+    quantity=int(quantity)
+    data = parse_json()
+    brother_cmd=data['brother_cmd']
+    for i in range(0,quantity):
+        return (os.system(brother_cmd + img))
+
+def parse_json():
+    with open('config.json') as f:
+        data = json.load(f)
+        return data
+
+def main():
+    data=parse_json()
+    host=data['localhost']
+    port=data['port']
+    if host==True:
+        app.run(port=port)
+    else:
+        app.run(host='0.0.0.0', port=port)
+    # this line should prevent exe file from closing.it allows server to keep running until the exe file is open
+    if input() == 'quit':
+        sys.exit(0)
+
+
+
+    return
+
+
+if __name__ == '__main__':
+    main()
